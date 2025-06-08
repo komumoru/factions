596f81189ed9741920e6f0d66867fc6e06b6cc11
Arona74 <alexandre.poete@gmail.com>
2025-06-08 18:37:45 +0200

    Guest permissions issue fixed by Paul-31415
    https://github.com/Paul-31415/factions
    Using code from master repo too (InteractionManager.java).

--- src/main/java/io/icker/factions/api/persistents/Relationship.java
+++ src/main/java/io/icker/factions/api/persistents/Relationship.java
@@ -4,6 +4,7 @@ import io.icker.factions.database.Field;
 
 import java.util.ArrayList;
 import java.util.UUID;
+import io.icker.factions.FactionsMod;
 
 public class Relationship {
     public enum Status {
@@ -28,7 +29,7 @@ public class Relationship {
     public Status status;
 
     @Field("Permissions")
-    public ArrayList<Permissions> permissions = new ArrayList<>();
+    public ArrayList<Permissions> permissions = new ArrayList<>(FactionsMod.CONFIG.RELATIONSHIPS.DEFAULT_GUEST_PERMISSIONS);
 
     public Relationship(UUID target, Status status) {
         this.target = target;

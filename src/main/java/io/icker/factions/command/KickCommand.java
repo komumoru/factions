2061851a577d7e57fd6f1f0c6fecefc3407d40f4
Arona74 <alexandre.poete@gmail.com>
2025-06-09 19:18:33 +0200

    Allow kicking offline players
    https://github.com/ickerio/factions/commit/b635026ba9db3e75e8944b121da445d271f776e2

--- src/main/java/io/icker/factions/command/KickCommand.java
+++ src/main/java/io/icker/factions/command/KickCommand.java
@@ -1,5 +1,7 @@
 package io.icker.factions.command;
 
+import com.mojang.authlib.GameProfile;
+import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.tree.LiteralCommandNode;
@@ -12,15 +14,34 @@ import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.Formatting;
+import java.util.Optional;
+import java.util.UUID;
 
 public class KickCommand implements Command {
     private int run(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
-        ServerPlayerEntity target = EntityArgumentType.getPlayer(context, "player");
-
         ServerCommandSource source = context.getSource();
         ServerPlayerEntity player = source.getPlayerOrThrow();
 
-        if (target.getUuid().equals(player.getUuid())) {
+        String name = StringArgumentType.getString(context, "player");
+
+        User target;
+
+        Optional<GameProfile> profile;
+        if ((profile = source.getServer().getUserCache().findByName(name)).isPresent()) {
+            target = User.get(profile.get().getId());
+        } else {
+            try {
+                target = User.get(UUID.fromString(name));
+            } catch (Exception e) {
+                new Message("No such player %s!", name)
+                        .format(Formatting.RED)
+                        .send(player, false);
+                return 0;
+            }
+        }
+
+        if (target.getID().equals(player.getUuid())) {
             new Message("Cannot kick yourself")
                 .fail()
                 .send(player, false);
@@ -28,10 +49,8 @@ public class KickCommand implements Command {
         }
 
         User selfUser = Command.getUser(player);
-        User targetUser = User.get(target.getUuid());
 
-        if (targetUser.getFaction() == null
-                || !targetUser.getFaction().equals(selfUser.getFaction())) {
+        if (target.getFaction() == null || !target.getFaction().equals(selfUser.getFaction())) {
             new Message("Cannot kick someone that is not in your faction")
                 .fail()
                 .send(player, false);
@@ -39,28 +58,40 @@ public class KickCommand implements Command {
         }
 
         if (selfUser.rank == User.Rank.LEADER
-                && (targetUser.rank == User.Rank.LEADER || targetUser.rank == User.Rank.OWNER)) {
+                && (target.rank == User.Rank.LEADER || target.rank == User.Rank.OWNER)) {
             new Message("Cannot kick members with a higher of equivalent rank")
                 .fail()
                 .send(player, false);
             return 0;
         }
 
-        targetUser.leaveFaction();
-        context.getSource().getServer().getPlayerManager().sendCommandTree(target);
+        ServerPlayerEntity targetPlayer =
+                player.getServer().getPlayerManager().getPlayer(target.getID());
 
-        new Message("Kicked " + player.getName().getString()).send(player, false);
-        new Message("You have been kicked from the faction by " + player.getName().getString())
-                .send(target, false);
+        target.leaveFaction();
+
+        if (targetPlayer != null) {
+            context.getSource().getServer().getPlayerManager().sendCommandTree(targetPlayer);
+
+            new Message("You have been kicked from the faction by %s",
+                                    player.getName().getString())
+                    .send(targetPlayer, false);
+        }
+
+        new Message("Kicked %s", profile.map((found_profile) -> found_profile.getName())
+                                        .orElse("unknown")).send(player, false);
 
         return 1;
     }
 
     public LiteralCommandNode<ServerCommandSource> getNode() {
         return CommandManager.literal("kick")
-                .requires(Requires.multiple(Requires.isLeader(),
+                .requires(
+                    Requires.multiple(Requires.isLeader(),
                         Requires.hasPerms("factions.kick", 0)))
-                .then(CommandManager.argument("player", EntityArgumentType.player())
+                .then(
+                    CommandManager.argument("player", EntityArgumentType.player())
+                        .suggests(Suggests.allPlayersInYourFactionButYou())
                         .executes(this::run))
                 .build();
     }
